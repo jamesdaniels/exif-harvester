d4f2ea64ab4b2e6a1d4310be605b9f06684d900f
jamesdaniels <james@marginleft.com>
2009-02-02 18:30:16 -0700

    Forgot to take out the dummy filename in the exiftool command.

--- harvester.rb
+++ harvester.rb
@@ -5,7 +5,7 @@ exif = {}
 
 `find #{base_folder} -name '#{search_pattern}'`.split("\n").each {|file|
 
-  exif[file.strip] = `exiftool /Users/james/Desktop/james.psd`.split("\n").collect{|a| 
+  exif[file.strip] = `exiftool #{file.strip}`.split("\n").collect{|a| 
     [
      (array = a.split(':')).first.strip, 
      (array.shift && array).join(':').strip
